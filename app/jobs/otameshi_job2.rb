c0c4efe8abeeac61824d2a9df94c651e00692bc8
strsbn <satorushibano@gmail.com>
2022-02-23 18:57:37 +0900

    綺麗綺麗

--- app/jobs/otameshi_job2.rb
+++ app/jobs/otameshi_job2.rb
@@ -2,6 +2,6 @@ class OtameshiJob2 < ApplicationJob
   queue_as :default
 
   def perform
-    puts 'おためし cron 実行 "cron: * * * * * Asia/Tokyo"'
+    puts 'おためし cron2 実行 "cron: * * * * * Asia/Tokyo"'
   end
 end
--- app/jobs/otameshi_job3.rb
+++ app/jobs/otameshi_job3.rb
@@ -2,6 +2,6 @@ class OtameshiJob3 < ApplicationJob
   queue_as :default
 
   def perform
-    puts 'おためし cron 実行 "cron: */5 * * * * Asia/Tokyo"'
+    puts 'おためし cron3 実行 "cron: */5 * * * * Asia/Tokyo"'
   end
 end
